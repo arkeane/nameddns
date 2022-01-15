1c3b9f1e8d3b9eb65f8ce107a6d3bebaa979f3a6
Ludovico Pestarino <ludovicopestarino00@gmail.com>
2022-01-15 16:46:41 +0100

    fix answer

--- nameddns.py
+++ nameddns.py
@@ -24,10 +24,9 @@ base_ip = requests.get('https://api.ipify.org?format=json')
 ip = base_ip.json()['ip']
 
 if ip != answer:
-    answer = ip
     # Write answer to config.json
     with open(os.path.join(__location__, 'config.json'), 'w') as outfile:
-        data['answer'] = answer
+        data['answer'] = ip
         json.dump(data, outfile, indent=4)
 
     # Make a request to the API
@@ -35,7 +34,7 @@ if ip != answer:
     url = base_url.format(domainName, id)
 
     # Create json data with host, type, answer and ttl
-    base_data = { "host": host, "type": type, "answer": answer, "ttl": ttl }
+    base_data = { "host": host, "type": type, "answer": ip, "ttl": ttl }
     data = json.dumps(base_data)
 
     # Make a request to the API
